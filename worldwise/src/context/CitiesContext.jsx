53e6250d4b21a67d8099b7efab6f5d47e410b92b
plamenggg <plamen_ganchev_94@abv.bg>
2024-09-23 17:10:16 +0300

    Implemented the delete city functionality

--- worldwise/src/context/CitiesContext.jsx
+++ worldwise/src/context/CitiesContext.jsx
@@ -55,10 +55,25 @@ function CitiesProvider({children}) {
         const data = await res.json();
         setCities(cities => [...cities, data]);
       } catch {
-        alert("There was an error loading data...");
+        alert("There was an error creating city.");
       } finally {
         setIsLoading(false);
       }
+}
+
+async function deleteCity(id) {
+  try {
+    setIsLoading(true);
+    await fetch(`${BASE_URL}/cities/${id}`, {
+      method: 'DELETE',
+      
+    }); 
+    setCities(cities => cities.filter(city => city.id !==id));
+  } catch {
+    alert("There was an error deleting city.");
+  } finally {
+    setIsLoading(false);
+  }
 }
 
     return (
@@ -69,6 +84,7 @@ function CitiesProvider({children}) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
     }}
     >
         {children}
